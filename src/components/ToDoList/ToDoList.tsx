312e31ebc48deda14b2497541bd4c2559bdd10f5
Eder Ramirez <eder.ramirez87@gmail.com>
2020-11-11 21:56:29 -0700

    styles(note-item): styles in note item

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -7,12 +7,14 @@ export const ToDoList: React.FC = () => {
   const [toDoList, setToDoList] = React.useState<IToDo[]>([
     {
       id: 1,
-      title: "title",
+      title:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate",
       checked: false,
     },
     {
       id: 2,
-      title: "2222",
+      title:
+        "Lorem ipsum dolor sitamet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
       checked: false,
     },
   ]);
--- src/components/ToDoList/ToDoListItem.tsx
+++ src/components/ToDoList/ToDoListItem.tsx
@@ -1,20 +1,24 @@
 import * as React from "react";
 import IToDo from "./IToDo";
 import Styles from "./toDoList.module.css";
+import { ReactComponent as MenuIcon } from "../../assets/menu.svg";
 
 export type ToDoListItemProps = { toDoItem: IToDo };
 export const ToDoListItem: React.FC<ToDoListItemProps> = ({ toDoItem }) => {
   console.log({ toDoItem });
   return (
     <div className={Styles.todoItem}>
-      <input
-        type="checkbox"
-        style={{ marginRight: "1rem" }}
-        name="todo-checked"
-        id="todo-checked"
-        checked={toDoItem.checked}
-      />
-      <p>{toDoItem.title}</p>
+      <div className={Styles.notes}>
+        <input
+          type="checkbox"
+          style={{ marginRight: "1rem" }}
+          name="todo-checked"
+          id="todo-checked"
+          checked={toDoItem.checked}
+        />
+        <p>{toDoItem.title}</p>
+      </div>
+      <MenuIcon className={Styles.menuIcon} />
     </div>
   );
 };
